bb4e70cc2516cc184d64fd6439a3c12facfbac0d
Amazon-Clone <monalisha.k559@gmail.com>
2026-04-11 11:31:26 +0530

    Question 4

---
+++ Python/Task4.py
@@ -0,0 +1,15 @@
+# Q4.
+
+def func():
+    dataset = ['amazon.in','gmail.com','python.py','flipkart.com']
+    d = {}
+
+    for data in dataset:
+        l = data.split('.')
+        if l[1] in d:
+            d[l[1]].append(l[0])
+        else:
+            d[l[1]] = [l[0]]
+    return d
+
+print(func())
